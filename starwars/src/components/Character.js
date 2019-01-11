3bd12759ecaf55fc0fea798090a2a5aa94ddf7a8
Olivia Osborn <olioz95@gmail.com>
2019-01-11 13:42:30 -0500

    added comments

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,6 +3,7 @@ import "./StarWars.css";
 
 function Character(props) {
     return (
+        // Renders out to a character component containing the name and birthday of each character
         <div className="character">
             <p className ="name" >{props.singleCharacter.name}</p>
             <p>Born in {props.singleCharacter.birth_year}</p>
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -4,6 +4,8 @@ import "./StarWars.css";
 
 function CharacterList (props) {
     return (
+        // map through data to create a new array of all characters
+        // for each character print out a Character component that lists name and birthday
         <div className="list">
             {props.characters.map(singleCharacter => {
                 return(
